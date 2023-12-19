162e6b37d7442ea5d1dcc21286ac44e1d8b1ae43
OmarNabawy <154077288+OmarNabawy@users.noreply.github.com>
2023-12-19 20:05:59 +0200

    Update and rename NNewapp.py to Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -23,57 +23,22 @@ def sequence_to_text(sequence, tokenizer):
     text = tokenizer.sequences_to_texts([sequence])[0]
     return text.strip()
 
-#def decrypt_text(model, input_text, tokenizer, max_length):
-#    input_seq = tokenizer.texts_to_sequences([input_text])
-#    input_seq = pad_sequences(input_seq, maxlen=max_length, padding="post", truncating="post")
-
- #   # Perform the prediction
- #   generated_sequence = model.predict(input_seq)
-
-  #  # Reshape the generated sequence to match the expected output
-  #  try:
-   #     generated_sequence = generated_sequence.reshape((generated_sequence.shape[1],))
-   # except ValueError as e:
-    #    print("Error during reshaping:", e)
-     #   print("Actual Generated Sequence Shape:", generated_sequence.shape)
-
-    # #Decrypt the generated sequence
-    #decrypted_text = sequence_to_text(generated_sequence.argmax(axis=-1)[0], tokenizer)
-
-    #return decrypted_text
-    
-def decrypt_text(model, input_text, tokenizer, max_length, temperature=1.0):
+def decrypt_text(model, input_text, tokenizer, max_length):
     input_seq = tokenizer.texts_to_sequences([input_text])
     input_seq = pad_sequences(input_seq, maxlen=max_length, padding="post", truncating="post")
 
     # Perform the prediction
     generated_sequence = model.predict(input_seq)
 
-    # Ensure that generated_sequence is a 1D array
-    generated_sequence = np.squeeze(generated_sequence)
-
     # Reshape the generated sequence to match the expected output
     try:
-        generated_sequence = generated_sequence.reshape((generated_sequence.shape[0],))
+        generated_sequence = generated_sequence.reshape((generated_sequence.shape[1],))
     except ValueError as e:
         print("Error during reshaping:", e)
         print("Actual Generated Sequence Shape:", generated_sequence.shape)
 
-    # Add print statements
-    print("Generated Sequence Shape:", generated_sequence.shape)
-
-    # Apply temperature to the generated sequence for sampling
-    generated_sequence = np.log(generated_sequence) / temperature
-    probabilities = np.exp(generated_sequence) / np.sum(np.exp(generated_sequence), axis=-1)
-
-    # Add print statements
-    print("Probabilities Shape:", probabilities.shape)
-
-    # Sample from the distribution to get the index of the predicted character
-    predicted_index = np.random.choice(len(probabilities), p=probabilities)
-    
-    # Convert the index to text using the tokenizer
-    decrypted_text = sequence_to_text([predicted_index], tokenizer)
+    #Decrypt the generated sequence
+    decrypted_text = sequence_to_text(generated_sequence.argmax(axis=-1)[0], tokenizer)
 
     return decrypted_text
 
