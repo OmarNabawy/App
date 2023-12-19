a76ff9d296bda8a1c8a40804b7839807a60ef8db
OmarNabawy <154077288+OmarNabawy@users.noreply.github.com>
2023-12-19 19:12:25 +0200

    Update Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
 import pickle
 import tensorflow as tf
 
@@ -9,21 +8,20 @@ def load_text_generation_model(model_path):
     model = tf.keras.models.load_model(model_path)
     return model
 
-text_generation_model = load_text_generation_model('LSTM_model.h5')
+def load_models():
+    # Load the text generation model
+    text_generation_model = load_text_generation_model('LSTM_model.h5')
 
-# Load the tokenizer used during training
-tokenizer_path = 'plaintext_tokenizer.pkl'  # Use the appropriate tokenizer file
-with open(tokenizer_path, 'rb') as f:
-    plaintext_tokenizer = pickle.load(f)
+    # Load the tokenizer used during training
+    tokenizer_path = 'plaintext_tokenizer.pkl'  # Use the appropriate tokenizer file
+    with open(tokenizer_path, 'rb') as f:
+        plaintext_tokenizer = pickle.load(f)
 
-max_length = 50
+    return text_generation_model, plaintext_tokenizer
 
 def decrypt_text(model, input_text, tokenizer, max_length):
     input_seq = tokenizer.texts_to_sequences([input_text])
     input_seq = pad_sequences(input_seq, maxlen=max_length, padding="post", truncating="post")
-
-    # Print the input sequence shape for debugging
-    print("Input Shape:", input_seq.shape)
 
     # Reshape the input sequence to match the model's input shape
     input_seq = input_seq.reshape((1, input_seq.shape[1], 1))
@@ -32,10 +30,15 @@ def decrypt_text(model, input_text, tokenizer, max_length):
     decrypted_text = tokenizer.sequences_to_texts(generated_sequence.argmax(axis=-1))
     return decrypted_text[0].strip()
 
-
-
 def main():
     st.title("Text Decryption App")
+
+    # Load the models
+    text_generation_model, plaintext_tokenizer = load_models()
+
+    # Print the model summary
+    st.subheader("Model Summary:")
+    st.text(text_generation_model.summary())
 
     # Upload encrypted text and key through Streamlit
     encrypted_text = st.text_input("Enter the encrypted text:")
@@ -46,11 +49,8 @@ def main():
         decrypted_text = decrypt_text(text_generation_model, encrypted_text, plaintext_tokenizer, max_length)
 
         # Display the decrypted text
-        st.markdown("Decrypted Text:")
+        st.subheader("Decrypted Text:")
         st.text(decrypted_text)
-
-text_generation_model.summary()
-
 
 if __name__ == "__main__":
     main()
